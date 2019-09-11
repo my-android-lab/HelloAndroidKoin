bf4d9f3a24bf60be937bad6ce3d12e5acb70ce96
sungcheol-kim <sungcheol@dable.io>
2019-09-11 10:31:16 +0900

    Inject a presenter to MainActivity

--- app/src/main/java/com/example/hellokoin/MainActivity.kt
+++ app/src/main/java/com/example/hellokoin/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.hellokoin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hellokoin.presenter.MainPresenter
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    private val presenter: MainPresenter by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
