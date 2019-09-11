4bef6f0a29b63c38a909d9505acf37b667eebe2e
sungcheol-kim <sungcheol@dable.io>
2019-09-11 12:28:45 +0900

    Test sayHello()

--- app/src/test/java/com/example/hellokoin/MainPresenterTest.kt
+++ app/src/test/java/com/example/hellokoin/MainPresenterTest.kt
@@ -1,7 +1,26 @@
 package com.example.hellokoin
 
+import com.example.hellokoin.module.appModule
+import com.example.hellokoin.presenter.MainPresenter
+import org.junit.Before
+import org.junit.Test
+import org.koin.core.context.startKoin
 import org.koin.test.KoinTest
+import org.koin.test.inject
 
 class MainPresenterTest: KoinTest {
-    
+
+    private val presenter: MainPresenter by inject()
+
+    @Before
+    fun before() {
+        startKoin {
+            modules(appModule)
+        }
+    }
+
+    @Test
+    fun testSayHello() {
+        assert(presenter.sayHello() == "Hello Koin")
+    }
 }
