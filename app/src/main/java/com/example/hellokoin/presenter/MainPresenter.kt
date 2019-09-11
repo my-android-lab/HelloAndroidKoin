1c2c36bdb1d1594655573b8e5f3c4070e4460a22
sungcheol-kim <sungcheol@dable.io>
2019-09-11 10:27:14 +0900

    Define a Presenter

---
+++ app/src/main/java/com/example/hellokoin/presenter/MainPresenter.kt
@@ -0,0 +1,7 @@
+package com.example.hellokoin.presenter
+
+import com.example.hellokoin.model.HelloRepository
+
+class MainPresenter(val repo: HelloRepository) {
+    fun sayHello() = "${repo.giveHello()} from $this"
+}
